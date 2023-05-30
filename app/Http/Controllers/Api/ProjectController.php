0927db6c2371ccf14e43296eade24ed83a57e4e4
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-30 09:44:55 +0200

    fix: project controller fixed

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,8 +10,15 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        // return al the Projects in the database
-        $projects = Project::all();
+        // return all the Projects in the database
+        // $projects = Project::all();
+
+        // here we are using the with method to eager load the type of the project  and the technologies of the project
+        // and then we are ordering them by the date of creation of the project
+        $projects = Project::with('type', 'technologies')
+            ->orderBy('projects.created_at', 'desc')
+            ->paginate(2);
+
 
         return response()->json([
             'success' => true,
